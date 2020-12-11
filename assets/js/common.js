44279dc56bacbacb410492f3bb6151d383600261
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2020-12-11 22:54:14 +0900

    js선택자명 수정 및 언어선택 기능 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,14 +1,25 @@
  $(document).ready(function () {
-  /* gnb 네비게이션 */
-  var $pcGnb = $('#pcGnb > ul');
-  var $dep2ul = $pcGnb.find('>li >ul');
+  /* lang_menu 숨기고 보이기 */
+  var $langBtn = $('.header .lang');
+  $langBtn.on({
+    'mouseenter focusin': function(){
+      $(this).next().addClass('on');
+    },
+    'mouseleave focusout' : function(){
+      $(this).next().removeClass('on');
+    }
+  });
+
+  /* gnb 네비게이션 Full down 효과 */
+  var $dep1ul = $('#pcGnb > ul');
+  var $dep2ul = $dep1ul.find('>li >ul');
 
   //1) depth2 ul  모두 숨기고 시작
   $dep2ul.hide();
 
   //2)depth1 ul에 마우스 진입과 나가기
-  $pcGnb.hover(
-    function () { //mouseente
+  $dep1ul.hover(
+    function () { //mouseenter
       // $('#pcGnb').addClass('on');
       $dep2ul.stop().slideDown();
     },
@@ -21,23 +32,23 @@
   );
 
   //3)depth1 li에 마우스, 포커스 진입
-  $pcGnb.children().on('mouseenter focusin', function () {
+  $dep1ul.children().on('mouseenter focusin', function () {
     $('#pcGnb').addClass('on');
     $dep2ul.stop().slideDown(); //포커스제어 필요
     $(this).addClass('on').siblings().removeClass('on');
   });
 
+  //열린 컨텐츠 초기화, li.on 제거
   function gnbReturn() {
-    //열려진 컨텐츠 초기화, li.on 제거
     $('#pcGnb').removeClass('on');
     $dep2ul.stop().slideUp();
-    $pcGnb.find('>li.on').removeClass('on');
+    $dep1ul.find('>li.on').removeClass('on');
   }
 
-  //5)첫번째와 마지막 a 에서 포커스가 떠나기
-  $pcGnb.find('a:first, a:last').on('blur', function () {
+  //5)첫번째, 마지막 a에서 포커스 떠나기
+  $dep1ul.find('a:first, a:last').on('blur', function () {
     setTimeout(function () {
-      if (!$pcGnb.find('a').is(':focus')) gnbReturn();
+      if (!$dep1ul.find('a').is(':focus')) gnbReturn();
     }, 10);
   });
 });
