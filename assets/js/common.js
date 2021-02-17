6702f36c41372dbf21f2f5cb0b9d2ced665b5e96
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-02-17 19:15:42 +0900

    mGnb-열고 닫기 기능 수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,133 +1,121 @@
 $(document).ready(function () {
-    /* 검색창 열기 버튼 */
-    //1) 버튼 눌러 열고 닫기
-    var $searchOpen = $('.header .search button');
-    var $searchForm = $('.header .search_wrap form')
-    $searchOpen.on('click', function(){
-        $searchForm.parent().slideToggle();
-    });
-    //2) 포커스 빠져나가거나 다른 곳 클릭할 시 닫히기
-    $searchForm.find('button').on('blur', function(){
-        setTimeout(function(){
-            if(!$searchForm.children().is(':focus')) {
-                $searchForm.parent().slideUp();
-                $searchOpen.focus();}
-        }, 10);
-    });
+	/* 검색창 열기 버튼 */
+	//1) 버튼 눌러 열고 닫기
+	var $searchOpen = $('.header .search button');
+	var $searchForm = $('.header .search_wrap form')
+	$searchOpen.on('click', function () {
+		$searchForm.parent().slideToggle();
+	});
+	//2) 포커스 빠져나가거나 다른 곳 클릭할 시 닫히기
+	$searchForm.find('button').on('blur', function () {
+		setTimeout(function () {
+			if (!$searchForm.children().is(':focus')) {
+				$searchForm.parent().slideUp();
+				$searchOpen.focus();
+			}
+		}, 10);
+	});
 
-    
-    /* pcGnb 네비게이션 Full down 효과 */
-    var $dep1ul = $('#pcGnb > ul');
-    var $dep2ul = $dep1ul.find('>li >ul');
+	/* PC메뉴 full down */
+	var $dep1ul = $('#pcGnb > ul');
+	var $dep2ul = $dep1ul.find('>li >ul');
 
-    //1) depth2 ul 모두 숨기고 시작
-    $dep2ul.hide();
+	//1) depth2 ul 모두 숨기고 시작
+	$dep2ul.hide();
 
-    //2)depth1 ul에 마우스 진입과 나가기
-    $dep1ul.hover(
-        function () { //mouseenter
-            // $('#pcGnb').addClass('on');
-            $dep2ul.stop().slideDown();
-        },
-        function () { //mouseleave
-            $dep2ul.stop().slideUp(function () {
-                gnbReturn();
-                // $('#pcGnb').removeClass('on');
-            }); //초기화면 활성화 필요
-        }
-    );
+	//2)depth1 ul에 마우스 진입과 나가기
+	$dep1ul.hover(
+		function () { //mouseenter
+			// $('#pcGnb').addClass('on');
+			$dep2ul.stop().slideDown();
+		},
+		function () { //mouseleave
+			$dep2ul.stop().slideUp(function () {
+				gnbReturn();
+				// $('#pcGnb').removeClass('on');
+			}); //초기화면 활성화 필요
+		}
+	);
 
-    //3)depth1 li에 마우스, 포커스 진입
-    $dep1ul.children().on('mouseenter focusin', function () {
-        $('#pcGnb').addClass('on');
-        $dep2ul.stop().slideDown(); //포커스제어 필요
-        $(this).addClass('on').siblings().removeClass('on');
-    });
+	//3)depth1 li에 마우스, 포커스 진입
+	$dep1ul.children().on('mouseenter focusin', function () {
+		$('#pcGnb').addClass('on');
+		$dep2ul.stop().slideDown(); //포커스제어 필요
+		$(this).addClass('on').siblings().removeClass('on');
+	});
 
-    //열린 컨텐츠 초기화, li.on 제거
-    function gnbReturn() {
-        $('#pcGnb').removeClass('on');
-        $dep2ul.stop().slideUp();
-        $dep1ul.find('>li.on').removeClass('on');
-    }
+	//열린 컨텐츠 초기화, li.on 제거
+	function gnbReturn() {
+		$('#pcGnb').removeClass('on');
+		$dep2ul.stop().slideUp();
+		$dep1ul.find('>li.on').removeClass('on');
+	}
 
-    $dep1ul.find('a:first, a:last').on('blur', function () {
-        setTimeout(function () {
-            if (!$dep1ul.find('a').is(':focus')) gnbReturn();
-        }, 10);
-    });
+	$dep1ul.find('a:first, a:last').on('blur', function () {
+		setTimeout(function () {
+			if (!$dep1ul.find('a').is(':focus')) gnbReturn();
+		}, 10);
+	});
 
-    /* 태블릿 메뉴 열기 */
-    var $tabdep1 = $('#mGnb > ul > li');
+	/* Tablet 메뉴 열기 */
+	$('#mHeader .menuBtn button').on('click', function () {
+		if (!$('#mGnb').hasClass('active')) {
+			$('#mGnb').addClass('active');
+			$('#mGnb').stop().slideDown();
 
-    $tabdep1.children('a').on('click', function(e){
-        e.preventDefault();
-        $(this).parent().addClass('active').find('ul').stop().slideDown();
-    });
+		} else {
+			$('#mGnb').removeClass('active');
+			$('#mGnb').stop().slideUp(function () {
+				$('#mGnb').find('li.active').removeClass('active').children('ul').slideUp();
+			});
+		} 
 
+		$('#mGnb > ul > li > a').on('click', function (e) {
+			e.preventDefault();
+			$(this).parent().siblings().removeClass('active').find('ul').stop().slideUp();
+			$(this).parent().toggleClass('active').find('ul').stop().slideToggle();
+		});
+	});
 
-    /* family site 옵션 열고 닫기 */
-    var $familyOpen = $('#familySite .f_btn');
-    var $familyLink = $('#familySite > ul > li')
-    var $f_siteGo = $('#familySite .fGo_btn');
+	/* family site 옵션 열고 닫기 */
+	var $familyOpen = $('#familySite .f_btn');
+	var $familyLink = $('#familySite > ul > li')
+	var $f_siteGo = $('#familySite .fGo_btn');
 
-    //1) 클릭하면 옵션 열고 닫기
-    $familyOpen.on('click', function(){
-        $(this).toggleClass('on').next('ul').stop().slideToggle();
+	//1) 클릭하면 옵션 열고 닫기
+	$familyOpen.on('click', function () {
+		$(this).toggleClass('on').next('ul').stop().slideToggle();
 
-        // 옵션에서 마우스가 나가면 자동 닫힘
-        $('#familySite').on('mouseleave', function(){
-            $familyOpen.next('ul').stop().slideUp();
-        });
-    });
+		// 옵션에서 마우스가 나가면 자동 닫힘
+		$('#familySite').on('mouseleave', function () {
+			$familyOpen.next('ul').stop().slideUp();
+		});
+	});
 
-    //2) 포커스 떠나면 옵션 닫기
-    $familyLink.children('a:first, a:last').on('blur', function(){
-        setTimeout(function(){
-            if(! $familyLink.children('a').is(':focus')) {
-                $familyOpen.removeClass('on').next('ul').stop().slideUp();
-            };
-        }, 10);
-    });
+	//2) 포커스 떠나면 옵션 닫기
+	$familyLink.children('a:first, a:last').on('blur', function () {
+		setTimeout(function () {
+			if (!$familyLink.children('a').is(':focus')) {
+				$familyOpen.removeClass('on').next('ul').stop().slideUp();
+			};
+		}, 10);
+	});
 
-    //3) a클릭할 시 텍스트, href변수에 담고 $familyOpen 글자 변경
-    var aHref = $familyLink.first().children('a').attr('href');
-    var aTxt = $familyLink.first().children('a').text();
-    
-    $familyLink.children('a').on('click', function(e){
-        e.preventDefault();
-        aHref = $(this).attr('href');
-        aTxt = $(this).text();
-        console.log(aHref, aTxt);
-        $familyOpen.text(aTxt).focus().next('ul').stop().slideToggle();
-    });
+	//3) a클릭할 시 텍스트, href변수에 담고 $familyOpen 글자 변경
+	var aHref = $familyLink.first().children('a').attr('href');
+	var aTxt = $familyLink.first().children('a').text();
 
-    //4) GO버튼 누르면 페이지 이동
-    $f_siteGo.on('click', function(e){
-        e.preventDefault();
-        window.open(aHref, 'popup');
-    });
+	$familyLink.children('a').on('click', function (e) {
+		e.preventDefault();
+		aHref = $(this).attr('href');
+		aTxt = $(this).text();
+		console.log(aHref, aTxt);
+		$familyOpen.text(aTxt).focus().next('ul').stop().slideToggle();
+	});
 
-
-    /* mGnb네비게이션 열고 닫기 */
-    var $mGnb1dep = $('#mGnb > ul');
-
-    //1) 1dep ul 열고 닫기
-    $('#mHeader .menuBtn button').on('click', function(){
-        if(! $(this).parent().hasClass('on')){
-            $(this).parent().addClass('on');
-        $mGnb1dep.stop().slideDown();
-
-        } else{
-            $(this).parent().removeClass('on');
-            $mGnb1dep.stop().slideUp(function(){
-                $mGnb1dep.find('li.on').removeClass('on').children('ul').slideUp();
-            });
-        }
-        //2) 2dep ul 열고 닫기
-        $('#mGnb > ul > li > a').on('click', function(e){
-            e.preventDefault();
-        $(this).next().slideToggle().parent().toggleClass('on').siblings().removeClass('on').children('ul').stop().slideUp();
-        });
-    });
+	//4) GO버튼 누르면 페이지 이동
+	$f_siteGo.on('click', function (e) {
+		e.preventDefault();
+		window.open(aHref, 'popup');
+	});
 });
